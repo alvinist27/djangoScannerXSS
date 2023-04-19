9d44aba248b800797ee6c6cfb8b7ba6c1b15a334
alvinist27 <suhrobhotamov496@gmail.com>
2023-04-19 17:03:44 +0300

    Added base views for scanner app

--- app_scanner/urls.py
+++ app_scanner/urls.py
@@ -1,1 +1,17 @@
 """URL configuration for app_scanner."""
+
+from django.urls import path
+
+from app_scanner import views
+
+urlpatterns = [
+    path('', views.main_view, name='main'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('404/', views.not_found, name='not_found'),
+    path('project/', views.project_view, name='project'),
+    path('service/', views.service_view, name='service'),
+    path('team/', views.team_view, name='team'),
+    path('testimonial/', views.testimonial_view, name='testimonial'),
+    path('scan/', views.scan_view, name='scan'),
+]
--- app_scanner/views.py
+++ app_scanner/views.py
@@ -1,1 +1,112 @@
 """Module for app_users views."""
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+
+def main_view(request: HttpRequest) -> HttpResponse:
+    """Display the main page of the scanner.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/index.html')
+
+
+def about_view(request: HttpRequest) -> HttpResponse:
+    """Display page with information about scanner.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/about.html')
+
+
+def contact_view(request: HttpRequest) -> HttpResponse:
+    """Display page with contact information.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/contact.html')
+
+
+def not_found(request: HttpRequest) -> HttpResponse:
+    """Display page for 404 error handling.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/404.html')
+
+
+def project_view(request: HttpRequest) -> HttpResponse:
+    """Display page with information about project.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/project.html')
+
+
+def service_view(request: HttpRequest) -> HttpResponse:
+    """Display page with information about service.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/service.html')
+
+
+def team_view(request: HttpRequest) -> HttpResponse:
+    """Display page with information about project.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/team.html')
+
+
+def testimonial_view(request: HttpRequest) -> HttpResponse:
+    """Display page with information about project.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/testimonial.html')
+
+
+def scan_view(request: HttpRequest) -> HttpResponse:
+    """Display scan page.
+
+    Args:
+        request: HttpRequest object.
+
+    Returns:
+        HttpResponse object.
+    """
+    return render(request, 'app_scanner/scan.html')
