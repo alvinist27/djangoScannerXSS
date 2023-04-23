5fdf6ec8941ac4af3af8fae366460e3ba1202469
alvinist27 <suhrobhotamov496@gmail.com>
2023-04-24 00:28:17 +0300

    Deleted redundant pages from templates

--- app_scanner/urls.py
+++ app_scanner/urls.py
@@ -7,11 +7,6 @@ from app_scanner import views
 urlpatterns = [
     path('', views.main_view, name='main'),
     path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
     path('404/', views.not_found, name='not_found'),
-    path('project/', views.project_view, name='project'),
-    path('service/', views.service_view, name='service'),
-    path('team/', views.team_view, name='team'),
-    path('testimonial/', views.testimonial_view, name='testimonial'),
-    path('scan/', views.ContactFormView.as_view(), name='scan'),
+    path('scan/', views.ScanFormView.as_view(), name='scan'),
 ]
--- app_scanner/views.py
+++ app_scanner/views.py
@@ -32,18 +32,6 @@ def about_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'app_scanner/about.html')
 
 
-def contact_view(request: HttpRequest) -> HttpResponse:
-    """Display page with contact information.
-
-    Args:
-        request: HttpRequest object.
-
-    Returns:
-        HttpResponse object.
-    """
-    return render(request, 'app_scanner/contact.html')
-
-
 def not_found(request: HttpRequest) -> HttpResponse:
     """Display page for 404 error handling.
 
@@ -56,55 +44,7 @@ def not_found(request: HttpRequest) -> HttpResponse:
     return render(request, 'app_scanner/404.html')
 
 
-def project_view(request: HttpRequest) -> HttpResponse:
-    """Display page with information about project.
-
-    Args:
-        request: HttpRequest object.
-
-    Returns:
-        HttpResponse object.
-    """
-    return render(request, 'app_scanner/project.html')
-
-
-def service_view(request: HttpRequest) -> HttpResponse:
-    """Display page with information about service.
-
-    Args:
-        request: HttpRequest object.
-
-    Returns:
-        HttpResponse object.
-    """
-    return render(request, 'app_scanner/service.html')
-
-
-def team_view(request: HttpRequest) -> HttpResponse:
-    """Display page with information about project.
-
-    Args:
-        request: HttpRequest object.
-
-    Returns:
-        HttpResponse object.
-    """
-    return render(request, 'app_scanner/team.html')
-
-
-def testimonial_view(request: HttpRequest) -> HttpResponse:
-    """Display page with information about project.
-
-    Args:
-        request: HttpRequest object.
-
-    Returns:
-        HttpResponse object.
-    """
-    return render(request, 'app_scanner/testimonial.html')
-
-
-class ContactFormView(FormView):
+class ScanFormView(FormView):
     """Display scan page."""
 
     form_class = ScanForm
